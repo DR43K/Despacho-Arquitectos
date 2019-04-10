63a488423560212aac872d4af26d3367d73daac4
RafaMac06 <rafamac_06@hotmail.com>
2019-04-10 08:54:09 -0500

    Principal Servicios finalizado

--- despacho-arquitectos/db/schema.rb
+++ despacho-arquitectos/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190409041723) do
+ActiveRecord::Schema.define(version: 20190410133446) do
 
   create_table "almacens", force: :cascade do |t|
     t.string "nombre"
@@ -62,6 +62,20 @@ ActiveRecord::Schema.define(version: 20190409041723) do
   create_table "pruebas", force: :cascade do |t|
     t.string "nombre"
     t.integer "precio"
+    t.string "categoria"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "services", force: :cascade do |t|
+    t.string "idservicio"
+    t.string "etiquetaservicio"
+    t.string "estadoventa"
+    t.string "estadocompra"
+    t.text "descripcion"
+    t.integer "duracion"
+    t.float "precioventa"
+    t.float "precioventamin"
     t.string "categoria"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
