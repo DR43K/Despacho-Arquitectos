0ab9456c17a560046a436cd2fd950b6e9b126b7e
CarlosAI <carloswesker@outlook.es>
2019-04-10 10:46:20 -0500

    LISTO

--- despacho-arquitectos/db/schema.rb
+++ despacho-arquitectos/db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20190410133446) do
 
-
   create_table "almacens", force: :cascade do |t|
     t.string "nombre"
     t.string "descripcion"
@@ -84,14 +83,6 @@ ActiveRecord::Schema.define(version: 20190410133446) do
     t.integer "duracion"
     t.float "precioventa"
     t.float "precioventamin"
-    t.string "categoria"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "pruebas", force: :cascade do |t|
-    t.string "nombre"
-    t.integer "precio"
     t.string "categoria"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
